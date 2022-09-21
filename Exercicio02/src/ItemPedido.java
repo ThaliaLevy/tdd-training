e7044ff7165a58dc2bd73e160ee6a047f1007642
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2022-09-21 20:12:44 -0300

    Testes em projeto que simula aplicação de desconto em itens
    Os itens possuem diferentes faixas de preços.

---
+++ Exercicio02/src/ItemPedido.java
@@ -0,0 +1,12 @@
+
+public class ItemPedido {
+	public String nomeProduto;
+	public double valor;
+	public int quantidade;
+
+	public ItemPedido(String nomeProduto, double valor, int quantidade) {
+		this.nomeProduto = nomeProduto;
+		this.valor = valor;
+		this.quantidade = quantidade;
+	}
+}
---
+++ Exercicio02/teste/PedidoTeste.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PedidoTeste {
+	private Pedido pedido;
+	
+	@Before
+	public void setup() {	
+		pedido = new Pedido();
+	}
+	
+	@Test
+	public void devePermitirAdicionarItemAoPedido() throws Exception {
+		pedido.adicionarItem(new ItemPedido("Shampoo", 15.0, 2));
+	}
+	
+	/*	forma 01:
+	@Test
+	public void deveCalcularValorTotalEDescontoParaPedidoVazio() throws Exception {
+		assertEquals(0.0, pedido.calcularTotalPedido(), 0.0001);
+		assertEquals(0.0, pedido.calcularDesconto(), 0.0001);
+	}
+	
+		forma 02:
+	*/
+	
+	public void assertResumoPedido(double valorTotal, double desconto) {
+		ResumoPedido resumoPedido = pedido.resumo();
+		assertEquals(valorTotal, resumoPedido.getValorTotal(), 0.0001);
+		assertEquals(desconto, resumoPedido.getDesconto(), 0.0001);
+	}
+	
+	@Test
+	public void deveCalcularValorTotalEDescontoParaPedidoVazio() throws Exception {
+		 assertResumoPedido(0.0, 0.0);
+	}
+	
+	@Test
+	public void deveCalcularValorTotalSemDescontoParaItem() throws Exception {
+		pedido.adicionarItem(new ItemPedido("Condicionador", 12.0, 2));
+		assertResumoPedido(24.0, 0.0);
+	}
+	
+	@Test
+	public void deveCalcularValorTotalDoisItensSemDesconto() throws Exception {
+		pedido.adicionarItem(new ItemPedido("Sabonete", 2.0, 3));
+		pedido.adicionarItem(new ItemPedido("Pasta de dente", 5.0, 5));
+		assertResumoPedido(31.0, 0.0);
+	}
+	
+	@Test
+	public void deveAplicarDescontoNa1aFaixa() throws Exception {
+		pedido.adicionarItem(new ItemPedido("Liquidificador", 100.0, 4));
+		assertResumoPedido(400.0, 16.0);
+	}
+	
+	@Test
+	public void deveAplicarDescontoNa2aFaixa() throws Exception {
+		pedido.adicionarItem(new ItemPedido("Máquina de lavar", 1000.0, 1));
+		assertResumoPedido(1000.0, 100.0);
+	}
+}
